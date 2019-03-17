f853b827a5b25501dc91f990430e290f04450005
Artyom87 <liborhuta@gmail.com>
2019-03-17 16:47:58 +0000

    Added script for lift rope to squas/stretch according to lift position. Added some more assets, including modular set for castle, created materials for castle and props.

---
+++ Assets/LiftRopeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftRopeScript : MonoBehaviour {
+
+	public GameObject lift;
+	private float scaleValue;
+	public float scaleMultiplier;
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector3 (1, 1, 1);
+		scaleMultiplier = 62.35F;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scaleValue = lift.transform.localPosition.y;
+		transform.localScale = new Vector3 (1, 1, 1 - scaleValue / scaleMultiplier);
+	}
+}
