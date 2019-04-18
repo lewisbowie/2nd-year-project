d6b63ba73ffddcd7e4c80e60de88d9386910ac75
UAD\1502642 <lewisbowie@outlook.com>
2019-04-18 14:56:12 +0100

    created some ui text animatin for tutorials

---
+++ Assets/FlyTextAppears.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class FlyTextAppears : MonoBehaviour {
+    public GameObject Fly;
+    private bool flyactive;
+	// Use this for initialization
+	void Start () {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       // flyactive = false;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (flyactive)
+       // {
+            if (collision.gameObject.tag == "Player")
+            {
+                Fly.gameObject.SetActive(true);
+
+            }
+        }
+   // }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!flyactive)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                Fly.gameObject.SetActive(false);
+
+            }
+        }
+    }
+}
